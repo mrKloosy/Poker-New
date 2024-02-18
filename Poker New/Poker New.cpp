cd295ed36701e1ebd52dcbbad520d073f74ef9e5
Kloosy <antonefrimov06@gmail.com>
2024-02-18 11:39:25 +0300

    Код с картами на столе и в руках

--- Poker New/Poker New.cpp
+++ Poker New/Poker New.cpp
@@ -7,6 +7,8 @@
 #include <ctime>
 #include <conio.h>
 #include <sstream>
+#include <algorithm>
+#include <random>
 using namespace std;
 vector <int> numbers;
 enum Color { Black = 0, Blue, Green, Cyan, Red, Magenta, Brown, LightGray, DarkGray, LightBlue, LightGreen, LightCyan, LightRed, LightMagenta, Yellow, White };
@@ -39,7 +41,7 @@ public:
 
 	void SetMasty(int Masty)
 	{
-		if (Masty >= 0 and Masty <= 3) //0-Черви, 1-Пики, 2-Бубны, 3-Крестье
+		if (Masty >= 0 and Masty <= 3) //0-Черви = C, 1-Пики = P, 2-Бубны = B, 3-Крестье = X
 		{
 			this->Masty = Masty;
 		}
@@ -88,16 +90,76 @@ public:
 		}
 	}
 
-	void printCardOnPos(int X, int Y)
+	void printCardOnPos(int X, int Y, Cards card, bool open)
 	{
 		SetCursor(X, Y);
-		for (size_t i = 0; i <= 5; i++)
+		for (size_t i = 0; i <= 6; i++)
 		{
 			for (size_t j = 0; j <= 6; j++)
 			{
-				if ((i == 0 or i == 5) or (j == 0 or j == 6))
+				if ((i == 0 or i == 6) or (j == 0 or j == 6))
 				{
 					cout << "*";
+				}
+				else if(i == 1 and j == 1 or i == 5 and j == 5)
+				{
+					if (open)  //0-Черви = C, 1-Пики = P, 2-Бубны = B, 3-Крестье = X
+					{
+						if (card.GetMasty() == 0)
+						{
+							SetColor(Red, Black);
+							cout << "C";
+							SetColor(White, Black);
+						}
+						else if (card.GetMasty() == 1)
+						{
+							cout << "P";
+						}
+						else if (card.GetMasty() == 2)
+						{
+							SetColor(Red, Black);
+							cout << "B";
+							SetColor(White, Black);
+						}
+						else if (card.GetMasty() == 3)
+						{
+							cout << "X";
+						}
+					}
+					else
+					{
+						cout << "?";
+					}
+				}
+				else if (i == 3 and j == 3)
+				{
+					if (open)
+					{
+						if (card.GetNomel() >= 2 and card.GetNomel() <= 10)
+						{
+							cout << card.GetNomel();
+						}
+						else if (card.GetNomel() == 11)
+						{
+							cout << "J";
+						}
+						else if (card.GetNomel() == 12)
+						{
+							cout << "Q";
+						}
+						else if (card.GetNomel() == 13)
+						{
+							cout << "K";
+						}
+						else if (card.GetNomel() == 14)
+						{
+							cout << "A";
+						}
+					}
+					else
+					{
+						cout << "?";
+					}
 				}
 				else
 				{
@@ -146,11 +208,65 @@ public:
 	}
 };
 
+void printMenu(vector <string> str, int punkt)
+{
+	int X = 15, Y = 5;
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		if (punkt == i)
+		{
+			SetColor(Magenta, Black);
+		}
+		else
+		{
+			SetColor(White, Black);
+		}
+		SetCursor(X, Y + i);//перед выводом
+		cout << str[i];
+
+	}
+	SetColor(White, Black);
+}
+
+int Menu(vector <string> str)
+{
+	int key = 0, punktOfMenu = 0;
+	do
+	{
+		printMenu(str, punktOfMenu);
+		key = _getch();
+		switch (key)
+		{
+		case Down:
+			if (punktOfMenu < str.size())
+			{
+				punktOfMenu++;
+
+			}
+			break;
+		case Up:
+			if (punktOfMenu > 0)
+			{
+				punktOfMenu--;
+			}
+			break;
+		case Enter:
+			return punktOfMenu;
+			break;
+		}
+
+	} while (key != Esc);
+	return -1;
+
+}
+
 int main()
 {
+	setlocale(LC_ALL, "RUS");
 	Cards card;
 	vector<Cards> cards(52);
-	int count = 0;
+	int count = 0, countCard = 0, bot1balance = 20000, bot2balance = 20000, bot3balance = 20000, playerbalance = 20000;
+	vector<string> menu = { "Начать игру", "Выход" };
 	for (int i = 0; i <= 3; i++)
 	{
 		for (size_t j = 2; j <= 14; j++)
@@ -166,41 +282,47 @@ int main()
 			}
 		}
 	}
-	/*for (int i = 0; i < 33; ++i)
-	{
-		cards[i].SetNomel((i % 13) + 2);
-		cards[i].SetMasty(i % 4);
-	}*/
 	Table t;
-	t.printCard();
+	
+	auto rng = default_random_engine{};
+	shuffle(cards.begin(), cards.end(), rng);
 	//карты стола
+	Menu(menu);
+	t.printCard();
 	for (size_t i = 18; i <= 50; i+=8)
 	{
-		t.printCardOnPos(i, 12);
+		t.printCardOnPos(i, 12, cards[countCard], true);
+		countCard++;
 	}
 	//карты левого бота
-	for (size_t i = 9; i <= 17; i+=8)
+	for (size_t i = 9; i <= 17; i += 8)
 	{
-		t.printCardOnPos(1, i);
+		t.printCardOnPos(1, i, cards[countCard], false);
+		countCard++;
 	}
 	//карты верхнего бота
 	for (size_t i = 29; i <= 41; i+= 12)
 	{
-		t.printCardOnPos(i, 1);
+		t.printCardOnPos(i, 1, cards[countCard], false);
+		countCard++;
 	}
 	//карты правого бота
 	for (size_t i = 9; i <= 17; i += 8)
 	{
-		t.printCardOnPos(66, i);
+		t.printCardOnPos(66, i, cards[countCard], false);
+		countCard++;
 	}
 	//наши карты
 	for (size_t i = 29; i <= 41; i += 12)
 	{
-		t.printCardOnPos(i, 24);
+		t.printCardOnPos(i, 24, cards[countCard], true);
+		countCard++;
 	}
-	SetCursor(0, 26);
-	for (int i = 0; i < 52; i++)
+	system("pause");
+	SetCursor(0, 28);
+	cout << "\n\n\n\n\n\n";
+	/*for (int i = 0; i < 52; i++)
 	{
 		cout << "Card " << i + 1 << ": Nomel = " << cards[i].GetNomel() << ", Masty = " << cards[i].GetMasty() << endl;
-	}
+	}*/
 }
